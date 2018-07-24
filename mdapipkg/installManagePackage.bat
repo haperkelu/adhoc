7493ea92f8a3442507baa5eb9462d45b48687520
Li <haperkelu@gmail.com>
2018-07-24 12:26:21 +1000

    init

---
+++ mdapipkg/installManagePackage.bat
@@ -0,0 +1,5 @@
+sfdx force:package:install -i 04t80000000cijSAAQ -u siraDX4ScratchOrg
+sfdx force:package:install -i 04t50000000EcdrAAC -u siraDX4ScratchOrg
+sfdx force:package:install -i 04t3A000000WuusQAC -u siraDX4ScratchOrg
+sfdx force:package:install -i 04t30000000bqOuAAI -u siraDX4ScratchOrg
+sfdx force:package:install -i 04t0Y000002NUHGQA4 -u siraDX4ScratchOrg
